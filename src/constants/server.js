75ad4b79dd3ab3627ea0521e8aa01327c89f2d21
Irvin Fiz <irvin.fiz.dev@gmail.com>
2021-01-29 22:13:05 -0600

    heroku

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,8 +1,14 @@
 export const localhost = 'http://localhost:4000/injection'
-export const url = 'https://production-server.vercel.app/injection'
+// export const url = 'https://production-server.vercel.app/injection'
+// export const url =
+//   'https://production-server.adrian-injection.vercel.app/injection'
+
+// export const url = 'https://prod-server.vercel.app/injection'
+
+export const url = 'https://prod-server.herokuapp.com/injection'
+
 export const options = {
   method: 'POST',
-  mode: 'cors',
   headers: {
     'Content-Type': 'application/json'
   }
